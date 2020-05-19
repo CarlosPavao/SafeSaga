65620b2a755744e65703167a348e2fad245c7896
CarlosPavao <carlos.henrique93@msn.com>
2020-05-19 00:50:45 -0300

    Inicio

---
+++ SafeSaga.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SafeSaga {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Bem Vindo ao Safe Saga.");
+
+        System.out.println("Introdução: ");
+    
+    
+    }
+    
+}
