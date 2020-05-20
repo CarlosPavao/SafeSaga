a2dbb47baa6727765652787942e118a9b9c934f3
alexsandrosr88 <63133361+alexsandrosr88@users.noreply.github.com>
2020-05-20 11:37:04 -0300

    Update SafeSaga.java

--- SafeSaga.java
+++ SafeSaga.java
@@ -1,17 +1,42 @@
 import java.util.Scanner;
 
-public class SafeSaga {
+/*
+ Projeto Integrador: Desenvolvimento de lógica
 
-    public static void main(String[] args) {
+ */
+
+/**
+ * Autores:
+• Alexsandro da Silva Ramos
+• Carlos Henrique Pavão Inácio
+• Patricia Ferreira de Sousa
+• Renan Ferreira Novaes
+• Rodrigo Dias
+• Vinicius Da Silva Alves
+ */
+public class NewClass {
+        public static void main(String[] args) {
+        
+    
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Bem Vindo ao Safe Saga.");
-
+        System.out.println("                Bem Vindo ao Safe Saga.");
+        System.out.println();
+        System.out.println();
         System.out.println("Introdução: ");
-        
-        System.out.print("Alexsandro passou por aqui");
-    
-    
-    }
-    
-}
+        System.out.println();
+        System.out.println();
+        System.out.println("A história do jogo se passa no início dos" +
+        " anos 2003, no início da popularização dos\n"+"computadores no Brasil,"
+         + " a história se passa na cidade de São Paulo.\n"
+         + "Safe Saga conta a história de um jovem que vive um grande desafio em sua\n" +
+"vida pois acaba tendo suas informações roubadas por um hacker mais o\n"
+                + "grande diferencial é que o hacker deseja apenas ensinar ao personagem\n" +
+"medidas de segurança da informação. Venha embarcar nesses desafios e\n"
+                + "aprender também sobre segurança da informação através de um jogo para\n" +
+"que consiga aplicar na sua própria vida os princípios ensinados aqui.\n"
+         + "O objetivo é ensinar a todos como manter nossas informações seguras em\n" +
+"tempos que cada vez mais estamos expostos a ataques hackers e perda de\n"
+                + "nossos dados que cada dia que passa se torna mais e mais importante.");
+            System.out.println();
+            System.out.println("       Venha se divertir com essa história envolvente.");
